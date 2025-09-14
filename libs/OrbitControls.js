b785e8ebc1eddabaea4d67019977349bf79ab135
tuccijr75 <michaelrobertucci@gmail.com>
2025-09-14 13:21:29 -0400

    Update OrbitControls.js

--- libs/OrbitControls.js
+++ libs/OrbitControls.js
@@ -1,1 +1,3 @@
-
+// Local proxy to OrbitControls (r159)
+export * from 'https://cdn.jsdelivr.net/npm/three@0.159.0/examples/jsm/controls/OrbitControls.js';
+export { default } from 'https://cdn.jsdelivr.net/npm/three@0.159.0/examples/jsm/controls/OrbitControls.js';
