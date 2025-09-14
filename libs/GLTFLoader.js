19973725b8c55deef87ed49c6ed4176381ebb997
tuccijr75 <michaelrobertucci@gmail.com>
2025-09-14 13:22:12 -0400

    Update GLTFLoader.js

--- libs/GLTFLoader.js
+++ libs/GLTFLoader.js
@@ -1,1 +1,3 @@
-
+// Local proxy to GLTFLoader (r159)
+export * from 'https://cdn.jsdelivr.net/npm/three@0.159.0/examples/jsm/loaders/GLTFLoader.js';
+export { default } from 'https://cdn.jsdelivr.net/npm/three@0.159.0/examples/jsm/loaders/GLTFLoader.js';
